019f3a7d76b30ebe2669aa9f05868d4353dfa0f3
Davi Silva <davisilvaphoto@gmail.com>
2024-08-31 16:36:16 -0300

    add controller

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,16 +1,19 @@
 import path from "node:path";
+import multer from "multer";
 
 import { Router } from "express";
-import multer from "multer";
 
 import { listCategories } from "./app/usecases/categories/list-categories";
 import { createCategory } from "./app/usecases/categories/create-category";
 import { listProducts } from "./app/usecases/products/list-products";
+
 import { createProduct } from "./app/usecases/products/create-product";
 import { listProductsByCategory } from "./app/usecases/categories/list-products-by-category";
+
 import { listOrders } from "./app/usecases/orders/list-orders";
 import { cancelOrder } from "./app/usecases/orders/cancel-order";
 import { createOrder } from "./app/usecases/orders/create-order";
+import { changeOrderStatus } from "./app/usecases/orders/change-order-status";
 
 export const router = Router();
 
@@ -34,13 +37,5 @@ router.post("/products", upload.single("image"), createProduct);
 
 router.get("/orders", listOrders);
 router.post("/orders", createOrder);
-
-// Change order status
-router.patch("/orders/:orderId", (req, res) => {
-  res.send("change order status");
-});
-
-// Delete/Cancel order
-router.delete("/orders/:orderId", (req, res) => {
-  res.send("cancel order");
-});
+router.patch("/orders/:orderId", changeOrderStatus);
+router.delete("/orders/:orderId", cancelOrder);
