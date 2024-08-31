fc7a053b3897d8258fa5f60ab5918d8ca1ec91eb
Davi Silva <davi.psilva@midway.com.br>
2024-08-31 15:54:29 -0300

    add static route to upload files

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,8 @@
+import path from "node:path";
+
 import express from "express";
 import mongoose from "mongoose";
+
 import { router } from "./routes";
 
 const PORT = 3001;
@@ -8,6 +11,11 @@ mongoose
   .connect("mongodb://admin:admin@localhost:27017")
   .then(() => {
     const app = express();
+
+    app.use(
+      "/uploads",
+      express.static(path.resolve(__dirname, "..", "uploads")),
+    );
 
     app.use(express.json());
     app.use(router);
