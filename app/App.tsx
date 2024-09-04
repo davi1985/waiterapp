59f3810d991aae8f673f200e71db2c18e1ebe920
Davi Silva <davisilvaphoto@gmail.com>
2024-09-04 09:13:01 -0300

    add custom text, prettier and custom fonts

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
-)
+import { useFonts } from 'expo-font'
+import { StatusBar } from 'expo-status-bar'
+
+const App = () => {
+  const [isFontsLoaded] = useFonts({
+    /* eslint-disable @typescript-eslint/no-require-imports */
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  })
+
+  return isFontsLoaded ? (
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'GeneralSans-400' }}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  ) : null
+}
 
 export default App
 
