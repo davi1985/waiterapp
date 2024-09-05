dd795be7afc8c232b34515ed2eb9c37280bd0458
Davi Silva <davisilvaphoto@gmail.com>
2024-09-05 18:22:28 -0300

    add products list

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,8 +1,50 @@
+import { FlatList } from 'react-native'
 import { Text } from '../Text'
-import { Container } from './styles'
+import {
+  Container,
+  Product,
+  ProductDetails,
+  ProductImage,
+  Separator,
+  AddToCartButton,
+} from './styles'
+import { products } from '../../mocks/products'
+import { formatCurrency } from '../../utils/formatCurrency'
+import { PlusCircle } from '../Icons/PlusCircle'
 
 export const Menu = () => (
   <Container>
-    <Text>Menu</Text>
+    <FlatList
+      data={products}
+      keyExtractor={({ _id }) => _id}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage
+            source={{
+              uri: `http://192.168.0.100:3001/uploads/${product.imagePath}`,
+            }}
+          />
+
+          <ProductDetails>
+            <Text weight="600">{product.name}</Text>
+
+            <Text color="#777" size={14} style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+
+            <Text size={14} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+      ItemSeparatorComponent={Separator}
+    />
   </Container>
 )
--- app/src/mocks/products.ts
+++ app/src/mocks/products.ts
@@ -3,7 +3,7 @@ export const products = [
     _id: '6372e040f52e37ef85fe2c5e',
     name: 'Pizza quatro queijos',
     description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1671336669119-frango-catupiry.png',
+    imagePath: '1725129903187-quatro-queijos.png',
     price: 40,
     ingredients: [
       {
@@ -32,7 +32,7 @@ export const products = [
     _id: '6372e276a381106c0f854cb3',
     name: 'Coca cola',
     description: 'Coca cola lata geladinha topzera',
-    imagePath: '1671343203320-coca-cola.png',
+    imagePath: '1725130825133-coca-cola.png',
     price: 7,
     ingredients: [],
   },
