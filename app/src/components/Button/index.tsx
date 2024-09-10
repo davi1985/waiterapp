661f3e091876d737ad8ce41681b3e91455cecba4
Davi Silva <davisilvaphoto@gmail.com>
2024-09-10 09:08:03 -0300

    add loading property

--- app/src/components/Button/index.tsx
+++ app/src/components/Button/index.tsx
@@ -1,17 +1,23 @@
 import { ReactNode } from 'react'
 import { Text } from '../Text'
 import { Container } from './styles'
+import { ActivityIndicator } from 'react-native'
 
 type Props = {
   children: ReactNode
   onPress: () => void
   disabled?: boolean
+  loading?: boolean
 }
 
-export const Button = ({ children, onPress, disabled }: Props) => (
-  <Container onPress={onPress} disabled={disabled}>
-    <Text weight="600" color="#fff">
-      {children}
-    </Text>
+export const Button = ({ children, onPress, disabled, loading }: Props) => (
+  <Container onPress={onPress} disabled={disabled || loading}>
+    {!loading ? (
+      <Text weight="600" color="#fff">
+        {children}
+      </Text>
+    ) : (
+      <ActivityIndicator color={'#fff'} />
+    )}
   </Container>
 )
